c4a57e0970726a3888d396dd20ea87e7f7634eed
Jerry <lujing2457@163.com>
2025-05-29 16:50:54 +0800

    chore: add build script

--- vite.renderer.config.mjs
+++ vite.renderer.config.mjs
@@ -3,5 +3,10 @@ import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config
 export default defineConfig({
-    plugins: [vue()]
+    plugins: [vue()],
+    base: './',
+    build: {
+        outDir: 'electron/pages',
+        // 打包的结果直接生成到 electron 的目录中去，这样electron 构建的时候，可以直接使用 index.html 了就
+    }
 });
